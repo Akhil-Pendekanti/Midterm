ae4751b15edcd3eb0fae449b25077dc85bbb767f
Akhil-Pendekanti <vp679@njit.edu>
2024-10-26 17:31:17 -0400

    logging values addtion

--- app/plugins/divide/__init__.py
+++ app/plugins/divide/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from app.commands import Command
 from app.history import History
 
@@ -8,11 +9,13 @@ class DivideCommand(Command):
             input1 = float(input("Enter the first number: "))
             input2 = float(input("Enter the second number: "))
             result = input1 / input2
-            print(result)
+            print('The result of the operation is',result)
+            logging.info(f'Division of {input1} and {input2} = {input1 / input2}')
+            logging.info('The Division operation was performed successfully')
             data = ['divide', input1, input2]
             existing_data = hist_inst.get_as_list()
             existing_data.append(data)
             hist_inst.writing_the_data(existing_data)
-            
+
         except ValueError:
                 print("Please enter a valid number!")
